626d9e95e7559db117664f656ca69647fab4960e
시혁 박 <exon.indiv@gmail.com>
2021-10-03 20:07:19 +0900

    Add: tailwind

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function App() {
     return (
-        <h1>Hi</h1>
+       
+       <div>
+           <h1 className="bg-black">Hi, My name is EXON</h1>
+           <button className="shadow-2xl">Hi</button>
+       </div>
     )
 }
 
