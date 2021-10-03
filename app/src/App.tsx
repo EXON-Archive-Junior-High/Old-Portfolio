963711d5d365d8e2bc8c9fd7d203c53d025227ae
시혁 박 <exon.indiv@gmail.com>
2021-10-03 21:20:05 +0900

    Add: vsc theme

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 
 function App() {
     return (
-        <div className="m-5">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#HashTag</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#HashTag</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#HashTag</span>
+        <div className="container m-10 ">
+            <div>
+                <img id="logo"></img>
+                <h1 className="text-4xl font-extralight"><span id="pink">import</span> <span id="red">EXON</span>;</h1>
+                <p className="font-light"><span id="pink">let</span> <span id="red">Student Developer</span><span id="green">:</span><span id="yellow"> UNCOMMON</span>;</p>
+            </div>
+            <div className="container mt-10">
+                <h1 className="text-4xl font-extralight"><span id="pink">class</span> <span id="yellow">Skills</span> &#123;</h1>
+                <p id="tap" className="text-2xl font-light">&#9;&#9; Typescript Javascript C# C C++</p>
+            </div>
         </div>
+        
     )
 }
 
